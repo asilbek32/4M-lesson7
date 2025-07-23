09c43544f79df0bf83447ddd8323ee1826a01744
asilbek32 <asilbekmahmudov93@gmail.com>
2025-07-23 21:32:33 +0500

    *

--- app.js
+++ app.js
@@ -22,13 +22,80 @@ function capitalaze() {
 
 console.log(capitalaze());
 
-//reduce
-Array.prototype.myreduce = function(cb){
 
-}
+Array.prototype.myReduce = function (callback, initialValue) {
+  let accumulator = initialValue;
+  let startIndex = 0;
+  if (accumulator === undefined) {
+    if (this.length === 0) {
+      return;
+    }
+    accumulator = this[0];
+    startIndex = 1;
+  }
 
+  for (let i = startIndex; i < this.length; i++) {
+    accumulator = callback(accumulator, this[i], i, this);
+  }
 
-let a = [1,4,56,7]
-const sum =a.reduce((acc, num) => acc+ num, 0)
+  return accumulator;
+};
 
+let a = [1, 3, 4, 5];
+let sum = a.myReduce((acc, num) => acc + num, 0);
 console.log(sum);
+
+Array.prototype.myFilter = function (callback) {
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
+
+const arr = [1, 2, 3, 4];
+const mF = arr.myFilter(num => num % 2 === 0);
+console.log(mF); 
+
+Array.prototype.mySome = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const arr1 = [1, 3, 5, 6];
+const hasEven = arr1.mySome(num => num % 2 === 0);
+console.log(hasEven); 
+
+Array.prototype.myEvery = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (!callback(this[i], i, this)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const arr2 = [2, 4, 63];
+const allEven = arr2.myEvery(num => num % 2 === 0);
+console.log(allEven); 
+
+Array.prototype.myFind = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      return this[i];
+    }
+  }
+
+  return undefined;
+};
+
+const arr4 = [5, 8, 7, 89];
+const found = arr4.myFind(num => num > 10);
+console.log(found); 
